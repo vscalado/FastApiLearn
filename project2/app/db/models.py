67faae0adc5a82a9bfb416700b655f9bad37a126
Vitor Calado <vitor.santos.calado@gmail.com>
2024-06-22 12:00:48 +0100

    Added Product Models and Migration

--- project2/app/db/models.py
+++ project2/app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, func
+from sqlalchemy.orm import relationship
 from app.db.base import Base
 
 class Category(Base):
@@ -6,5 +7,16 @@ class Category(Base):
     id = Column('id',Integer, primary_key=True, autoincrement=True)
     name = Column('name', String(50), nullable=False)
     slug = Column('slug', String(100), nullable=False)
+    products = relationship('Product', back_populates='category')
 
-    
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column('id',Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String(50), nullable=False)
+    slug = Column('slug', String(100), nullable=False)
+    price = Column('price', Float)
+    stock = Column('stock', Integer)
+    created_at = Column('created_at', DateTime, server_default=func.now())
+    updated_at = Column('updated_at', DateTime, onupdate=func.now())
+    category_id = Column('category_id', ForeignKey('categories.id'), nullable=False)
+    category = relationship('Category', back_populates='products')
